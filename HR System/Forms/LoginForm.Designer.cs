e3ef139b1eed2466efc9964e88a4c998f0708c79
bartekk.garus@gmail.com <Mikolaj1/>
2023-05-04 11:16:25 +0200

    loginform start

---
+++ HR System/Forms/LoginForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace HR_System
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rightPabel = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.rightPabel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rightPabel
+            // 
+            this.rightPabel.BackColor = System.Drawing.Color.Coral;
+            this.rightPabel.Controls.Add(this.loginButton);
+            this.rightPabel.Controls.Add(this.titleLabel);
+            this.rightPabel.Controls.Add(this.passwordLabel);
+            this.rightPabel.Controls.Add(this.loginLabel);
+            this.rightPabel.Controls.Add(this.passwordTextBox);
+            this.rightPabel.Controls.Add(this.loginTextBox);
+            this.rightPabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rightPabel.Location = new System.Drawing.Point(316, 0);
+            this.rightPabel.Name = "rightPabel";
+            this.rightPabel.Size = new System.Drawing.Size(484, 450);
+            this.rightPabel.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(169, 305);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(162, 33);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.titleLabel.Location = new System.Drawing.Point(45, 101);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(426, 41);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "Zaloguj się na swoje konto";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordLabel.Location = new System.Drawing.Point(107, 257);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 21);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Hasło:";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loginLabel.Location = new System.Drawing.Point(107, 198);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(56, 21);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "Login:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordTextBox.Location = new System.Drawing.Point(169, 254);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(162, 29);
+            this.passwordTextBox.TabIndex = 1;
+            this.passwordTextBox.PasswordChar = '*';
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTextBox.Location = new System.Drawing.Point(169, 195);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(162, 29);
+            this.loginTextBox.TabIndex = 0;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(316, 450);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rightPabel);
+            this.Controls.Add(this.leftPanel);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.rightPabel.ResumeLayout(false);
+            this.rightPabel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel rightPabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label loginLabel;
+    }
+}
+
---
+++ HR System/Forms/LoginForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR_System
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private string password => passwordTextBox.Text;
+
+        private string login => loginTextBox.Text;
+
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if(password == "")
+            {
+
+            }
+        }
+    }
+}
